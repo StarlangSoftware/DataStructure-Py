e7c86eadedfb2fd5c2012fb55caf7302d2d35a34
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:30:49 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-DataStructure',
-    version='1.0.9',
+    version='1.0.10',
     packages=['DataStructure', 'DataStructure.Cache'],
     url='https://github.com/StarlangSoftware/DataStructure-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
-    description='Simple Data Structures Library'
+    description='Simple Data Structures Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
